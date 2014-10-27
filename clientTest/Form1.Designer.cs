4822aa1d59d2f84520be755fdd8b1be41bd3471d
Rencie Bautista <rencie.bautista@yahoo.com>
2014-10-27 22:18:52 +0800

    first commit

---
+++ clientTest/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace clientTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtReply = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtReply1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(13, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtReply
+            // 
+            this.txtReply.Location = new System.Drawing.Point(95, 13);
+            this.txtReply.Multiline = true;
+            this.txtReply.Name = "txtReply";
+            this.txtReply.Size = new System.Drawing.Size(397, 334);
+            this.txtReply.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 4000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtReply1
+            // 
+            this.txtReply1.Location = new System.Drawing.Point(498, 12);
+            this.txtReply1.Multiline = true;
+            this.txtReply1.Name = "txtReply1";
+            this.txtReply1.Size = new System.Drawing.Size(397, 334);
+            this.txtReply1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 359);
+            this.Controls.Add(this.txtReply1);
+            this.Controls.Add(this.txtReply);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtReply;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtReply1;
+    }
+}
+
---
+++ clientTest/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace clientTest
+{
+    public partial class Form1 : Form
+    {
+        UdpClient udpClient = new UdpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Connect()
+        {
+            return "<?xml version=\"1.0\"?><Tig><Client.Connect Name=\"AVLS\" Version=\"2.12\" /></Tig>";
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            sendUdp();
+        }
+
+        private void sendUdp()
+        {
+            // UdpClient udpClient = new UdpClient();
+            //udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true); // reuse port
+            //udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, 30512));
+
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(Connect());
+            try
+            {
+                udpClient.Send(sendBytes, sendBytes.Length, "192.168.1.180", 30511); // send data to server via port 30511
+
+
+
+                Thread thdUDPServer = new Thread(new ThreadStart(serverThread)); //start thread for recieving
+                if (thdUDPServer.ThreadState != ThreadState.Running)
+                {
+                    thdUDPServer.Start();
+                }
+                
+
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sendUdp();
+        }
+        public void serverThread()
+        {
+            try
+            {
+                udpClient.BeginReceive(new AsyncCallback(recv), null);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        //CallBack
+        private void recv(IAsyncResult res)
+        {
+            //IPAddress tnxip = IPAddress.Parse("192.168.1.180");
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.180"), 0);
+            byte[] received = udpClient.EndReceive(res, ref RemoteIpEndPoint);
+
+            //Process codes
+
+            //MessageBox.Show(Encoding.UTF8.GetString(received));
+            this.Invoke((MethodInvoker)(() => txtReply.AppendText(RemoteIpEndPoint +"=>"+Encoding.UTF8.GetString(received) + Environment.NewLine)));
+            udpClient.BeginReceive(new AsyncCallback(recv), null);
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true); // reuse port
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, 30512));
+            //try
+            //{
+            //    udpClient.BeginReceive(new AsyncCallback(recv), null);
+            //}
+            //catch(Exception ee)
+            //{
+            //     MessageBox.Show(ee.ToString());
+            //}  
+        }
+
+        //CallBack
+        //private void recv(IAsyncResult res)
+        //{
+        //    try
+        //    {
+        //        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        //        byte[] received = udpClient.EndReceive(res, ref RemoteIpEndPoint);
+
+        //        //Process codes
+
+        //        // MessageBox.Show(Encoding.UTF8.GetString(received));
+        //        //txtReply.AppendText(Encoding.UTF8.GetString(received));
+        //        this.Invoke((MethodInvoker)(() => txtReply.AppendText(Encoding.UTF8.GetString(received))));
+        //        udpClient.BeginReceive(new AsyncCallback(recv), null);
+        //    }
+
+        //    catch (Exception ee)
+        //    {
+        //        MessageBox.Show(ee.ToString());
+        //    } 
+        //}
+
+        public void serverThread_1()
+        {
+            UdpClient udpClient = new UdpClient();
+            while (true)
+            {
+                //IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                //Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                //string returnData = Encoding.ASCII.GetString(receiveBytes);
+                // lbConnections.Items.Add(RemoteIpEndPoint.Address.ToString() + ":" + returnData.ToString());
+                // this.Invoke((MethodInvoker)(() => lbConnections.Items.Add(RemoteIpEndPoint.Address.ToString() + ":" + returnData.ToString())));
+
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+                //txtReply.Text = returnData;
+                this.Invoke((MethodInvoker)(() => txtReply1.Text = returnData.ToString()));
+            }
+        }
+    }
+}
